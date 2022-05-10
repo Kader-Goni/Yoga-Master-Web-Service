67586cf0ab695a6e1002aae304f3786d37eab112
Kader-Goni <kadergoni35@gmail.com>
2022-05-10 21:44:56 +0600

    footer added

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copyright &copy; 2022. All rights reserved By Kader Goni.</p>
+        </div>
+    );
+};
+
+export default Footer;
